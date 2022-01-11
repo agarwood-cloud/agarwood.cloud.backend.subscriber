bd24dc3743827c97b14af368730d73634ea54b2c
agarwood <676786620@qq.com>
2022-01-11 11:13:24 +0800

    chore:delete getting-started module

--- src/app/pages/getting-started/getting-started.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'da-getting-started',
-  template: ` <router-outlet></router-outlet> `,
-})
-export class GettingStartedComponent {}
